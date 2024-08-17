c9678c657e0532628528da7479a85ea6a756ce6d
Abdul Raheem <abdulraheembeigh@gmail.com>
2024-08-17 15:50:26 +0100

    Initial Commit

---
+++ JPMorganChallenge/JPMorganChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPMorganChallengeApp.swift
+//  JPMorganChallenge
+//
+//  Created by Abdul Raheem Beigh on 17/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct JPMorganChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
